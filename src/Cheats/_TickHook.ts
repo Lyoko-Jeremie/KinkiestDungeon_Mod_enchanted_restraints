bd31ae917069fea160e227bf376d2174b05393e1
LyokoJeremie <lucheng989898@gmail.com>
2024-02-06 16:00:00 +0800

    TickHook

--- src/Cheats/_TickHook.ts
+++ src/Cheats/_TickHook.ts
@@ -34,13 +34,14 @@ export class TickHook {
             throw Error('TickHook Hook_HookFunctionCaller() (!this.Hook_HookedFunction_Point).');
         }
         this.Hook_HookedFunction_Point!.bind(this.windowPtr)(...arg);
+        console.log('TickHook Hook_HookedFunction_Point ', arg[0]);
         // a slow speed filter for hook point `KDProcessInput`
-        if (arg[0] === "move" || arg[0] === 'tick') {
-            // console.log('TickHook Hook_HookedFunction_Point Hook_HookFunctionCaller', this.Hook_HookedFunction_Point, this.Hook_Map);
-            this.Hook_Map.forEach((v, k) => {
-                v();
-            });
-        }
+        // if (arg[0] === "move" || arg[0] === 'tick') {
+        // console.log('TickHook Hook_HookedFunction_Point Hook_HookFunctionCaller', this.Hook_HookedFunction_Point, this.Hook_Map);
+        this.Hook_Map.forEach((v, k) => {
+            v();
+        });
+        // }
     }
 
     addHook(callback: CallableFunction) {
