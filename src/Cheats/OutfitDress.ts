6becf5b7f86f10e3d1d2312bb373385809ada9b2
LyokoJeremie <lucheng989898@gmail.com>
2025-01-26 05:55:50 +0800

    .

---
+++ src/Cheats/OutfitDress.ts
@@ -0,0 +1,30 @@
+
+// KinkyDungeonDress.ts
+
+// KDGetDressList()
+// KDModelDresses
+//// KinkyDungeonDresses
+
+// /**
+//  * It sets the player's appearance based on their stats.
+//  *
+//  * @param [Character]
+//  * @param [NoRestraints]
+//  * @param [Force]
+//  * @param [npcRestraints]
+//  * @param [customInventory]
+//  * @param [customPlayerTags]
+//  * @param [customFaction]
+//  * @param [noDressOutfit]
+//  */
+// function KinkyDungeonDressPlayer (
+//     Character?:         Character,
+//     NoRestraints?:      boolean,
+//     Force?:             boolean,
+//     npcRestraints?:     Record<string, NPCRestraint>,
+//     customInventory?:   item[],
+//     customPlayerTags?:  Map<string, boolean>,
+//     customFaction?:     string,
+//     noDressOutfit?:     boolean
+// )
+
