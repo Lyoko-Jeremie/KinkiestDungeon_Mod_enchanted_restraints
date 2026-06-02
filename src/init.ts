20094be2a1dc5738c4dd7818c45f048e05138b54
LyokoJeremie <lucheng989898@gmail.com>
2026-06-02 17:25:04 +0800

    ..

--- src/init.ts
+++ src/init.ts
@@ -1,12 +1,14 @@
 import './polyfill';
 // import './GreasemonkeyScript/CreateGui';
 // import {CreateGui} from './GreasemonkeyScript/CreateGui';
-import {CreateGui} from './GUI_PortableUi/CreatePortableUi';
+// import {CreateGui} from './GUI_PortableUi/CreatePortableUi';
+import {CreateGui} from './GUI_PortableSimpleUi/CreatePortableSimpleUi';
 // import {CreateGuiTweakPane} from "./GuiTweakPane";
 
-import {createModZone} from '@PortableUi/adaptor/ModZone';
+// import {createModZone} from '@PortableUi/adaptor/ModZone';
+import {createZoneWrapper} from '@PortableSimpleUi';
 
-window.gEnchantedRestraintsCreateGui_modZone = createModZone();
+window.gEnchantedRestraintsCreateGui_modZone = createZoneWrapper('EnchantedRestraintsCreateGui');
 
 if (!window.gEnchantedRestraintsCreateGui) {
     window.gEnchantedRestraintsCreateGui_modZone.runGuarded(() => {
